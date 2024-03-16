a9d45198a1f54686653c55309ad0b6a6c08ca043
alemoles <alemolerio@gmail.com>
2024-03-16 20:36:30 -0300

    set default app orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:speed_indicator_flutter/presentation/screens/screens.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ],
+  ).then(
+    (value) => runApp(
+      const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
