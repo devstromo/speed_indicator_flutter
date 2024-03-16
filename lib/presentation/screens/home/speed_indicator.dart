22a8f048a45a38a7321ad0daa5b04d7946f76d07
alemoles <alemolerio@gmail.com>
2024-03-16 20:27:22 -0300

    create home screen widget

---
+++ lib/presentation/screens/home/speed_indicator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SpeedIndicator extends StatelessWidget {
+  const SpeedIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: Placeholder(),
+          )
+        ],
+      ),
+    );
+  }
+}
