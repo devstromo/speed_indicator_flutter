20620a97a7db898628ff8abfbcf8080e9dcc57e8
alemoles <alemolerio@gmail.com>
2024-03-16 20:44:22 -0300

    add slider to control values

--- lib/presentation/screens/home/speed_indicator.dart
+++ lib/presentation/screens/home/speed_indicator.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
 
-class SpeedIndicator extends StatelessWidget {
+class SpeedIndicator extends StatefulWidget {
   const SpeedIndicator({super.key});
 
   @override
+  State<SpeedIndicator> createState() => _SpeedIndicatorState();
+}
+
+class _SpeedIndicatorState extends State<SpeedIndicator> {
+  double _value = 0.0;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Stack(
         children: [
-          Center(
+          const Center(
             child: Placeholder(),
-          )
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 10,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Slider(
+                value: _value,
+                min: 0.0,
+                max: 200.0,
+                onChanged: (value) {
+                  setState(() {
+                    _value = value;
+                  });
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
