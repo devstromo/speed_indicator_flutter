1dcd91133d052646252cc07de661c6e03e06f7d7
alemoles <alemolerio@gmail.com>
2024-03-17 15:17:41 -0300

    create speed indicator painter

---
+++ lib/presentation/widgets/speed_indicator_painter.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SpeedIndicatorPainter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+  }
+
+  @override
+  bool shouldRepaint(SpeedIndicatorPainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(SpeedIndicatorPainter oldDelegate) => false;
+}
