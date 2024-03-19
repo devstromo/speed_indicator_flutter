6d3548966e360a12f55202825abf486d8cd45144
alemoles <alemolerio@gmail.com>
2024-03-18 22:52:11 -0300

    update angle formula

--- lib/presentation/screens/home/speed_indicator.dart
+++ lib/presentation/screens/home/speed_indicator.dart
@@ -16,11 +16,7 @@ class _SpeedIndicatorState extends State<SpeedIndicator> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    // Calculate the rotation angle based on the slider value
-    // final double angle = math.pi * _value / 200;
-
-    final double angle = (math.pi * 2.05) - (_value * (math.pi / 6) / 220);
-    log('Angle value: $angle');
+    final double angle = (math.pi * 2.05) - (_value * (math.pi * 1.21) / 220);
     return Scaffold(
       body: SafeArea(
         child: Stack(
