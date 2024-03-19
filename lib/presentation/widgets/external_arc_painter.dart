13c7e45a09871e36952c6a31c5e75ef6d030d30e
alemoles <alemolerio@gmail.com>
2024-03-18 23:03:39 -0300

    add initial velocity indicators

--- lib/presentation/widgets/external_arc_painter.dart
+++ lib/presentation/widgets/external_arc_painter.dart
@@ -18,6 +18,47 @@ class ExternalArcPainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2;
     canvas.drawArc(rect, startAngle, sweepAngle, useCenter, paint);
+    // Text painting code starts here
+    final textStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    );
+
+    // The radius might need to be adjusted depending on the size of your canvas
+    final radius = size.width / 2;
+
+    // Start drawing numbers from 0 to 220 at intervals of 20 km/h
+    for (int i = 0; i <= 220; i += 20) {
+      // Angle calculation for the numbers must be adjusted to match your arc
+      // This assumes the numbers are spread over the same angle as the arc itself
+      final numberAngle = startAngle + (sweepAngle / 220) * i;
+
+      // Calculating the position for the text
+      final offset = Offset(
+        radius * math.cos(numberAngle) + (size.width / 2),
+        radius * math.sin(numberAngle) + (size.height / 2),
+      );
+
+      // Offset adjustment might be necessary to properly position the numbers
+      final textSpan = TextSpan(
+        text: '$i',
+        style: textStyle,
+      );
+      final textPainter = TextPainter(
+        text: textSpan,
+        textDirection: TextDirection.ltr,
+      );
+
+      textPainter.layout();
+      final textOffset = Offset(
+        offset.dx - (textPainter.width / 2),
+        offset.dy - (textPainter.height / 2),
+      );
+
+      // Draw the text on the canvas
+      textPainter.paint(canvas, textOffset);
+    }
   }
 
   @override
