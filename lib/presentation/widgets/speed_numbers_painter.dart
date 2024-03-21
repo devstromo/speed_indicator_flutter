20cdd153b90d0cad30ea428c6d42c3afdda9d00a
alemoles <alemolerio@gmail.com>
2024-03-20 21:38:21 -0300

    update speed number painter speed line

--- lib/presentation/widgets/speed_numbers_painter.dart
+++ lib/presentation/widgets/speed_numbers_painter.dart
@@ -11,32 +11,24 @@ class SpeedNumbersPainter extends CustomPainter {
       fontSize: 20,
       fontWeight: FontWeight.bold,
     );
-    // Paint for the lines
+
     final linePaint = Paint()
       ..color = Colors.black
       ..strokeWidth = 2;
 
-    // The radius might need to be adjusted depending on the size of your canvas
     final radius = size.width * .4;
     final lineLength = size.width * 0.03; // Adjust the length as needed
     final innerRadius =
         radius - lineLength; // This will be the start of the line
 
+    final dxOffsetDelta = (size.width * .5);
+    final dyOffsetDelta = (size.height * .68);
     int marker = 220;
-    // Start drawing numbers from 0 to 220 at intervals of 20 km/h
+
+    // Draw lines and numbers together in one loop
     for (int i = 0; i <= 220; i += 20) {
-      // Angle calculation for the numbers and lines
       final numberAngle = startAngle + (sweepAngle / 220) * i;
 
-      final dxOffsetDelta = (size.width * .5);
-      final dyOffsetDelta = (size.height * .68);
-      // Calculating the position for the text
-      final textOffset = Offset(
-        radius * math.cos(numberAngle) + dxOffsetDelta,
-        radius * math.sin(numberAngle) + dyOffsetDelta,
-      );
-
-      // Calculating the start and end points for the lines
       final lineStartOffset = Offset(
         innerRadius * math.cos(numberAngle) + dxOffsetDelta,
         innerRadius * math.sin(numberAngle) + dyOffsetDelta,
@@ -49,7 +41,6 @@ class SpeedNumbersPainter extends CustomPainter {
       // Draw the line on the canvas
       canvas.drawLine(lineStartOffset, lineEndOffset, linePaint);
 
-      // Text painting setup
       final textSpan = TextSpan(
         text: '$marker',
         style: textStyle,
@@ -61,14 +52,18 @@ class SpeedNumbersPainter extends CustomPainter {
 
       textPainter.layout();
 
-      // Adjust text position to center it on the line
-      final adjustedTextOffset = Offset(
-        textOffset.dx - (textPainter.width * .5),
-        textOffset.dy - (textPainter.height * .5),
+      // Calculate the position for the number, it should be on the outside of the line end
+      final numberOffset = Offset(
+        (innerRadius + lineLength + 20) * math.cos(numberAngle) +
+            dxOffsetDelta -
+            textPainter.width * .5,
+        (innerRadius + lineLength + 20) * math.sin(numberAngle) +
+            dyOffsetDelta -
+            textPainter.height * .5,
       );
 
-      // Draw the text on the canvas
-      textPainter.paint(canvas, adjustedTextOffset);
+      // Draw the text on the canvas, ensuring it's on top of the line
+      textPainter.paint(canvas, numberOffset);
       marker -= 20; // Decrease marker for next number
     }
   }
