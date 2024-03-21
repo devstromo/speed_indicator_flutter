8b1e027f605783920be933bbc8e5963c00133ce5
alemoles <alemolerio@gmail.com>
2024-03-20 21:23:48 -0300

    add line markers for speed

--- lib/presentation/widgets/speed_numbers_painter.dart
+++ lib/presentation/widgets/speed_numbers_painter.dart
@@ -6,30 +6,50 @@ class SpeedNumbersPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     const startAngle = -math.pi * 1.85;
     const sweepAngle = -math.pi * 1.30;
-
     const textStyle = TextStyle(
       color: Colors.black,
       fontSize: 20,
       fontWeight: FontWeight.bold,
     );
+    // Paint for the lines
+    final linePaint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 2;
 
     // The radius might need to be adjusted depending on the size of your canvas
     final radius = size.width * .4;
+    final lineLength = size.width * 0.1; // Adjust the length as needed
+    final innerRadius =
+        radius - lineLength; // This will be the start of the line
 
     int marker = 220;
     // Start drawing numbers from 0 to 220 at intervals of 20 km/h
     for (int i = 0; i <= 220; i += 20) {
-      // Angle calculation for the numbers must be adjusted to match your arc
-      // This assumes the numbers are spread over the same angle as the arc itself
+      // Angle calculation for the numbers and lines
       final numberAngle = startAngle + (sweepAngle / 220) * i;
 
       // Calculating the position for the text
-      final offset = Offset(
+      final textOffset = Offset(
         radius * math.cos(numberAngle) + (size.width * .5),
         radius * math.sin(numberAngle) + (size.height * .68),
       );
 
-      // Offset adjustment might be necessary to properly position the numbers
+      final dxOffsetDelta = (size.width * .5);
+      // Calculating the start and end points for the lines
+      final lineStartOffset = Offset(
+        innerRadius * math.cos(numberAngle) + (size.width * .5),
+        innerRadius * math.sin(numberAngle) + (size.height * .68),
+      );
+      final lineEndOffset = Offset(
+        (innerRadius + lineLength) * math.cos(numberAngle) + (size.width * .5),
+        (innerRadius + lineLength) * math.sin(numberAngle) +
+            (size.height * .68),
+      );
+
+      // Draw the line on the canvas
+      canvas.drawLine(lineStartOffset, lineEndOffset, linePaint);
+
+      // Text painting setup
       final textSpan = TextSpan(
         text: '$marker',
         style: textStyle,
@@ -40,14 +60,16 @@ class SpeedNumbersPainter extends CustomPainter {
       );
 
       textPainter.layout();
-      final textOffset = Offset(
-        offset.dx - (textPainter.width * .5),
-        offset.dy - (textPainter.height * .5),
+
+      // Adjust text position to center it on the line
+      final adjustedTextOffset = Offset(
+        textOffset.dx - (textPainter.width * .5),
+        textOffset.dy - (textPainter.height * .5),
       );
 
       // Draw the text on the canvas
-      textPainter.paint(canvas, textOffset);
-      marker -= 20;
+      textPainter.paint(canvas, adjustedTextOffset);
+      marker -= 20; // Decrease marker for next number
     }
   }
 
