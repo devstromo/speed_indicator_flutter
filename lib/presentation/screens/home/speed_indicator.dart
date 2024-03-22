88b45a3b0bb260e797e0b2a0dcd694166e1b310c
alemoles <alemolerio@gmail.com>
2024-03-21 22:26:21 -0300

    calculate needle size automatically

--- lib/presentation/screens/home/speed_indicator.dart
+++ lib/presentation/screens/home/speed_indicator.dart
@@ -13,17 +13,35 @@ class SpeedIndicator extends StatefulWidget {
 
 class _SpeedIndicatorState extends State<SpeedIndicator> {
   double _value = 220.0;
+  final midValue = 110.0;
+  final minFactor = 0.40;
+  final maxFactor = 0.45;
+
+  double _calculateLeftFactor(double value) {
+    if (value <= midValue) {
+      // Calculate factor for values from 0 to 110
+      double slope = (maxFactor - minFactor) / midValue;
+      return minFactor + slope * value;
+    } else {
+      // Calculate factor for values from 110 to 220
+      double slope = (minFactor - maxFactor) / (220 - midValue);
+      return maxFactor + slope * (value - midValue);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final double angle = (math.pi * 2.17) /** ok this first part */ - (_value * (math.pi * 1.34) / 220);
+    final double angle = (math.pi * 2.17) /** ok this first part */ -
+        (_value * (math.pi * 1.34) / 220);
+    final double leftFactor = _calculateLeftFactor(_value);
     const verticalPadding = 2.0;
     return Scaffold(
       body: SafeArea(
         child: Stack(
           children: [
             Positioned(
-              left: size.height * .45, // needle size
+              left: size.height * leftFactor, // needle size .40 in borders
               right: size.width * .5,
               bottom: kToolbarHeight + 60,
               child: SpeedIndicatorNeedle(
